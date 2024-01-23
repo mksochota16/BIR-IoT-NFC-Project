55d112076e4965f6aca11eebe6612c703951ef2c
Paweł Gryka <pawel.m.gryka@gmail.com>
2024-01-23 18:59:58 +0100

    Update arduino code with working HTTPS

--- Arduino/reader_and_sender/reader_and_sender.ino
+++ Arduino/reader_and_sender/reader_and_sender.ino
@@ -1,3 +1,29 @@
+#define CA_CERT "-----BEGIN CERTIFICATE-----\n" \
+"MIIEADCCAuigAwIBAgIID+rOSdTGfGcwDQYJKoZIhvcNAQELBQAwgYsxCzAJBgNV\n"  \
+"BAYTAlVTMRkwFwYDVQQKExBDbG91ZEZsYXJlLCBJbmMuMTQwMgYDVQQLEytDbG91\n"  \
+"ZEZsYXJlIE9yaWdpbiBTU0wgQ2VydGlmaWNhdGUgQXV0aG9yaXR5MRYwFAYDVQQH\n"  \
+"Ew1TYW4gRnJhbmNpc2NvMRMwEQYDVQQIEwpDYWxpZm9ybmlhMB4XDTE5MDgyMzIx\n"  \
+"MDgwMFoXDTI5MDgxNTE3MDAwMFowgYsxCzAJBgNVBAYTAlVTMRkwFwYDVQQKExBD\n"  \
+"bG91ZEZsYXJlLCBJbmMuMTQwMgYDVQQLEytDbG91ZEZsYXJlIE9yaWdpbiBTU0wg\n"  \
+"Q2VydGlmaWNhdGUgQXV0aG9yaXR5MRYwFAYDVQQHEw1TYW4gRnJhbmNpc2NvMRMw\n"  \
+"EQYDVQQIEwpDYWxpZm9ybmlhMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKC\n"  \
+"AQEAwEiVZ/UoQpHmFsHvk5isBxRehukP8DG9JhFev3WZtG76WoTthvLJFRKFCHXm\n"  \
+"V6Z5/66Z4S09mgsUuFwvJzMnE6Ej6yIsYNCb9r9QORa8BdhrkNn6kdTly3mdnykb\n"  \
+"OomnwbUfLlExVgNdlP0XoRoeMwbQ4598foiHblO2B/LKuNfJzAMfS7oZe34b+vLB\n"  \
+"yrP/1bgCSLdc1AxQc1AC0EsQQhgcyTJNgnG4va1c7ogPlwKyhbDyZ4e59N5lbYPJ\n"  \
+"SmXI/cAe3jXj1FBLJZkwnoDKe0v13xeF+nF32smSH0qB7aJX2tBMW4TWtFPmzs5I\n"  \
+"lwrFSySWAdwYdgxw180yKU0dvwIDAQABo2YwZDAOBgNVHQ8BAf8EBAMCAQYwEgYD\n"  \
+"VR0TAQH/BAgwBgEB/wIBAjAdBgNVHQ4EFgQUJOhTV118NECHqeuU27rhFnj8KaQw\n"  \
+"HwYDVR0jBBgwFoAUJOhTV118NECHqeuU27rhFnj8KaQwDQYJKoZIhvcNAQELBQAD\n"  \
+"ggEBAHwOf9Ur1l0Ar5vFE6PNrZWrDfQIMyEfdgSKofCdTckbqXNTiXdgbHs+TWoQ\n"  \
+"wAB0pfJDAHJDXOTCWRyTeXOseeOi5Btj5CnEuw3P0oXqdqevM1/+uWp0CM35zgZ8\n"  \
+"VD4aITxity0djzE6Qnx3Syzz+ZkoBgTnNum7d9A66/V636x4vTeqbZFBr9erJzgz\n"  \
+"hhurjcoacvRNhnjtDRM0dPeiCJ50CP3wEYuvUzDHUaowOsnLCjQIkWbR7Ni6KEIk\n"  \
+"MOz2U0OBSif3FTkhCgZWQKOOLo1P42jHC3ssUZAtVNXrCk3fw9/E15k8NPkBazZ6\n"  \
+"0iykLhH1trywrKRMVw67F44IE8Y=\n"\
+"-----END CERTIFICATE-----\n"
+//ca of cloudflare
+
 #include "WiFiS3.h"
 #include <Wire.h>
 #include <PN532_I2C.h>
@@ -18,8 +44,9 @@ String payload_str;
 byte nuidPICC[4];
 
 int status = WL_IDLE_STATUS;
-IPAddress server_ip(192,168,222,86);  // numeric IP
-int server_port = 80; // port 80 is the default for HTTP
+//IPAddress server_ip(89,76,210,123);  // numeric IP
+char server[] = "host";
+int server_port = 443; // port 80 is the default for HTTP
 
 WiFiSSLClient client;
 
@@ -28,7 +55,7 @@ void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.println("Start setup");
-  
+
   digitalWrite(LED_BUILTIN, HIGH);
 
   // check for the WiFi module:
@@ -96,7 +123,7 @@ void read_response() {
 
 
 void post_request() {
-  while (!client.connectSSL(server_ip, server_port)) {
+  while (!client.connect(server, server_port)) {
     Serial.println("Connection failed, trying again...");
     delay(100);
   }
@@ -106,7 +133,7 @@ void post_request() {
   client.print("POST /sensor-reading?reading=");
   client.print(payload_str.substring(14));
   client.println(" HTTP/1.1");
-  client.println("Host: 192.168.222.86");//IP once again
+  client.println("Host: host");//IP once again
   client.println("accept: application/json");
   client.println("Connection: close");
   client.println();
